cbdd6495996bc85e1d38ed862b1269a65aa65cc0
Khonsadiya Nasywa Salsabila <nasywa9905@gmail.com>
2026-03-03 21:08:59 +0700

    Tambahkan otentikasi Breeze, halaman about dengan controller, route, dan navbar link

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200">
+            About
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 text-gray-900 dark:text-gray-100">
+                <p>Nama : Khonsadiya Nasywa Salsabila</p>
+                <p>NIM : 20230140112</p>
+                <p>Program Studi : Teknologi Informasi</p>
+                <p>Hobi : (isi hobbimu)</p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
